9dacb538e2f22c885d10a02d74b010b90bc3fc9c
Iain Barnett <iainspeed@gmail.com>
2010-08-19 04:32:50 +0100

    format may not be given

--- lib/Names.rb
+++ lib/Names.rb
@@ -4,7 +4,7 @@ module RandomPerson
   
   module Names
 
-    def Names.execute( names, format )
+    def Names.execute( names, format=nil )
       format = ->(n){ n.rand } if format.nil?
       # puts format.inspect
       format.( names )
