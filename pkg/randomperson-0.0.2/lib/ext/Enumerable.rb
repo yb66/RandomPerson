95d4f98817408c2b5b56a60638a9e35fcb31b0aa
Iain Barnett <iainspeed@gmail.com>
2010-08-24 17:47:50 +0100

    removed last gemfiles

--- pkg/randomperson-0.0.2/lib/ext/Enumerable.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby -wKU
-
-#add fold to the available method names
-module Enumerable
-  alias :fold inject unless Enumerable.method_defined? :fold
-end
--- pkg/randomperson-0.0.2/lib/ext/Hash.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby -wKU
-
-
-class Hash
-  def by_index( n )
-    self.each_with_index { |item, index| break item if index == n }
-  end
-end
-  
