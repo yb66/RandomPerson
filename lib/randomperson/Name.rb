e3e8bb01a1bc4fe60cbb2e9c533c15a244f8b16f
Iain Barnett <iainspeed@gmail.com>
2010-08-25 14:25:16 +0100

    removed assumption that an single hash entry would be called 'single'

--- lib/randomperson/Name.rb
+++ lib/randomperson/Name.rb
@@ -10,7 +10,7 @@ module RandomPerson
         i = @formats_ratiod.index_in_range( r )
         f = @formats.by_index(i).last
       else
-        f = @formats[:single]
+        f = @formats.first
       end
       f.( @names )
     end 
