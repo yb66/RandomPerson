18e1cce99c6984438f6a5c1c43f1562d7feb85af
Iain Barnett <iainspeed@gmail.com>
2010-10-02 17:15:29 +0100

    added German names

---
+++ lib/randomperson/Names/GermanFemaleFirst.rb
@@ -0,0 +1,21 @@
+module RandomPerson
+  module Names
+
+    class GermanFemaleFirst < Name
+  
+      def initialize
+        @formats = {
+          :single =>                      ->(n)   { n.rand },
+         }
+         
+        @formats_ratiod = [ (0..99) ]
+        setupnames
+      end
+        
+      def setupnames
+        @names = %w{ Andrea Angelika Anja Anke Anna Annett Antje Barbara Birgit Brigitte Christin Christina Claudia Daniela Diana Doreen Franziska Gabriele Heike Ines Jana Janina Jennifer Jessica Julia Juliane Karin Karolin Katharina Kathrin Katja Kerstin Klaudia Kristin Laura Lea Lena Lisa Mandy Manuela Maria Marie Marina Martina Melanie Monika Nadine Nicole Petra Sabine Sabrina Sandra Sara Silke Simone Sophia Stefanie Susanne Tanja Ulrike Ursula Uta Vanessa Yvonne }
+      end
+    end    
+  end
+end
+ 
---
+++ lib/randomperson/Names/GermanMaleFirst.rb
@@ -0,0 +1,21 @@
+module RandomPerson
+  module Names
+
+    class GermanMaleFirst < Name
+  
+      def initialize
+        @formats = {
+          :single =>                      ->(n)   { n.rand },
+         }
+         
+        @formats_ratiod = [ (0..99) ]
+        setupnames
+      end
+        
+      def setupnames
+        @names = %w{ Alexander Andreas Benjamin Bernd Christian Daniel David Dennis Dieter Dirk Dominik Eric Felix Florian Frank Jan Jens Jonas Jörg Jürgen Kevin Klaus Kristian Leon Lukas Marcel Marco Mario Markus Martin Mathias Max Maximilian Michael Mike Niklas Patrick Paul Peter Philipp Ralf René Robert Sebastian Stefan Steffen Sven Thomas Thorsten Tim Tobias Tom Ulrich Uwe Wolfgang }
+      end
+    end    
+  end
+end
+ 
