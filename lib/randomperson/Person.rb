8ec97ebd36de67caaf6124e53830305beae00646
Iain Barnett <iainspeed@gmail.com>
2011-05-29 19:02:13 +0100

    added some yard comments

--- lib/randomperson/Person.rb
+++ lib/randomperson/Person.rb
@@ -1,8 +1,18 @@
 module RandomPerson
+  
+  # Your general person
   class Person
   
     attr_accessor :gender, :age, :dob, :first, :last, :prefix, :suffix
     
+    ## @param [optional,Hash] h A hash of the person's attributes
+    # @option h [String] :gender nil Gender, 'm' for male or 'f' for female
+    # @option h [Integer] :age nil Age
+    # @option h [Time] :dob nil Date of birth
+    # @option h [String] :first nil First name
+    # @option h [String] :last nil Last name
+    # @option h [String] :prefix nil Prefix e.g. Mr
+    # @option h [String] :suffix nil Suffix e.g. PhD
     def initialize( h={} )
         @gender = h[:gender] || nil
         @age = h[:age] || nil
