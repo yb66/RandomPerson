f9e387cfa3e7ecb8f9a5a939c511f9e8c4a59aee
Iain Barnett <iainspeed@gmail.com>
2011-06-13 16:00:52 +0100

    updated requires

--- lib/randomperson.rb
+++ lib/randomperson.rb
@@ -1,17 +1,22 @@
-#require all the scaffolding
+# encoding: UTF-8
+module RandomPerson
 
-require 'set'
-require 'date'
+  #require all the scaffolding
+  
+  require 'set'
+  require 'date'
+  
+  require_relative './randomperson/ext/Array.rb'
+  require_relative './randomperson/ext/Date.rb'
+  require_relative './randomperson/ext/Enumerable.rb'
+  require_relative './randomperson/ext/Hash.rb'
+  require_relative './randomperson/ext/Kernel.rb'
+  require_relative './randomperson/ext/Set.rb'
+  
+  require_relative './randomperson/choice.rb'
+  require_relative './randomperson/generator.rb'
+  require_relative './randomperson/name.rb'
+  require_relative './randomperson/person.rb'
+  #require_relative './randomperson/ratio.rb'
 
-require_relative 'randomperson/ext/Array.rb'
-require_relative 'randomperson/ext/Date.rb'
-require_relative 'randomperson/ext/Enumerable.rb'
-require_relative 'randomperson/ext/Hash.rb'
-require_relative 'randomperson/ext/Kernel.rb'
-require_relative 'randomperson/ext/Set.rb'
-
-require_relative 'randomperson/choice.rb'
-require_relative 'randomperson/generator.rb'
-require_relative 'randomperson/name.rb'
-require_relative 'randomperson/person.rb'
-#require_relative 'randomperson/ratio.rb'
+end
