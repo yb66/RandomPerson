4395a8c96316dee1a5451a6913bbda60e874ca3a
Iain Barnett <iainspeed@gmail.com>
2011-09-20 17:51:53 +0100

    added simplecov

--- spec/rspec_helper.rb
+++ spec/rspec_helper.rb
@@ -1,6 +1,9 @@
 # encoding: UTF-8
 
 require "rspec"
+require 'simplecov'
+
+SimpleCov.start
 
 spec_dir = File.expand_path( File.dirname __FILE__ )
 Dir[ File.join( spec_dir, "/support/**/*.rb")].each {|f| require f}
