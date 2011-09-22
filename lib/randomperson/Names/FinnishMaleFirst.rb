25b60ac253e1ec5d2097315a1c60608024287b17
Iain Barnett <iainspeed@gmail.com>
2011-09-22 15:33:20 +0100

    updated Finnish male names. with spec

--- lib/randomperson/Names/FinnishMaleFirst.rb
+++ lib/randomperson/Names/FinnishMaleFirst.rb
@@ -1,22 +1,23 @@
 # encoding: UTF-8
 
+require_relative "../name.rb"
+require_relative "../ext/Array.rb"
+
 module RandomPerson
   module Names
 
     class FinnishMaleFirst < Name
   
       def initialize
-        @formats = {
-          :single =>                      ->(n)   { n.rand },
-         }
-         
+        @names = Names
+        @formats = [
+          ->(n)   { n.rand },
+         ]
         @formats_ratiod = [ (0..99) ]
-        setupnames
+        super
       end
         
-      def setupnames
-        @names = %w{ Antero Ilmari Johannes Juhani Kalevi Matti Mikael Olavi Tapani Tapio }
-      end
+      Names = %w{ Antero Ilmari Johannes Juhani Kalevi Matti Mikael Olavi Tapani Tapio }
     end    
   end
 end
