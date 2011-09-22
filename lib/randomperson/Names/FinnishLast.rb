db09db04b805e3de8032cadba063b84740cff104
Iain Barnett <iainspeed@gmail.com>
2011-09-22 15:22:24 +0100

    updated Finnish last names. with spec

--- lib/randomperson/Names/FinnishLast.rb
+++ lib/randomperson/Names/FinnishLast.rb
@@ -1,22 +1,23 @@
 # encoding: UTF-8
 
+require_relative "../name.rb"
+require_relative "../ext/Array.rb"
+
 module RandomPerson
   module Names
 
     class FinnishLast < Name
       
       def initialize
-        @formats = {
-          :single =>                      ->(n)   { n.rand }
-         }
-       
+        @names = Names
+        @formats = [
+          ->(n)   { n.rand }
+         ]
         @formats_ratiod = [ (0..99) ]
-        setupnames
+        super
       end  
       
-      def setupnames
-        @names = %w( Heikkinen Hämäläinen Järvinen Korhonen Koskinen Laine Mäkelä Mäkinen Nieminen Virtanen )
-      end
+      Names = %w( Heikkinen Hämäläinen Järvinen Korhonen Koskinen Laine Mäkelä Mäkinen Nieminen Virtanen )
       
     end 
   end
