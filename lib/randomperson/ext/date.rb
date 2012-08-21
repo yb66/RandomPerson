a7a5204700d38e989d6614f79afb4f8633116932
Iain Barnett <iainspeed@gmail.com>
2012-08-21 18:34:52 +0100

    Removed open class, probably don't need it defined like this anyway but will look at later.

--- lib/randomperson/ext/date.rb
+++ lib/randomperson/ext/date.rb
@@ -1,6 +1,13 @@
-class Date 
-  def Date.days_in_month( year, month ) 
-    Date.civil(year, month, -1).day 
-  end  
-end
+module RandomPerson
+  class Date < ::Date
 
+    # Convenience function
+    # @param [Integer] year
+    # @param [Integer] month
+    # @return [Integer] 
+    def self.days_in_month( year, month ) 
+      ::Date.civil(year, month, -1).day 
+    end  
+
+  end
+end
